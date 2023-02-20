72647b354bfde50d251a372311229e17e02ccdbe
akka97 <akapellari@yahoo.com>
2023-02-20 04:31:04 -0800

    printalphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lower case
+ * Used char only twice
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n')
+	return (0);
+}
