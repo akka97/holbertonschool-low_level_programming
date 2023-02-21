8392c8640164f1aa2498bb5575b25118b144138b
akka97 <akapellari@yahoo.com>
2023-02-21 00:47:07 -0800

    printnumbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lower and upper case
+ * Used char only three times
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 0; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
