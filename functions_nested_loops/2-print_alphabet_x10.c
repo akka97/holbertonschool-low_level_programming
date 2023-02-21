0a06d7d334cc9330f0ccf1e66ba2fcba49d4f7e4
akka97 <akapellari@yahoo.com>
2023-02-21 04:51:09 -0800

    printalphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_x10
- * prints ten times the alphabet
- * No return
+ * print_alphabet_x10-prints ten times the alphabet
+ *
+ * return: no return
  */
 
 void print_alphabet_x10(void)
