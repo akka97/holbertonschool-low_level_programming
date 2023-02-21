f2e1a2fe8a3f6766ff3a0852ace58c893053e2a8
akka97 <akapellari@yahoo.com>
2023-02-21 01:50:40 -0800

    printcomb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lower and upper case
+ * Used char only three times
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
