3e6c406a93f63144e5b888eabb3895e448979189
akka97 <akapellari@yahoo.com>
2023-02-21 04:39:24 -0800

    printalphabet10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+
+			_putchar('\n');
+			i++;
+	}
+}
