667344b59dd69695fca2b2ea0b0f1374c75317d8
akka97 <akapellari@yahoo.com>
2023-02-22 04:37:47 -0800

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs
+ * @n: positive number
+ * Return: 1 and print + if n is greater than zero, 0 and print
+ * 0 if n is zero, return -1 nad print - if n is less than zero
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = -1 * i;
+		return (i);
+	}
+	else
+	{
+		return (i);
+	}
+}
