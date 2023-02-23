35c84867db8519ca9be47d141c4813b67cb06f9b
akka97 <akapellari@yahoo.com>
2023-02-23 03:02:42 -0800

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - adding two values
+* @n1: First number to be computed
+* @n2: Second number to be computed
+* Return: Empty output
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
