16724f9ad14b46c3caaa55fdf3adaf09b39823e3
akka97 <akapellari@yahoo.com>
2023-02-24 03:56:11 -0800

    printmostnumbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_most_numbers - print numbers from zero to nine, expect two and four
+*
+* Return: Zero.
+*/
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		if (num != 2 && num != 4)
+		_putchar(num);
+	}
+
+	_putchar('\n');
+}
