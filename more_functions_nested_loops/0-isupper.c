29239d349aeef076555a2390ae125076a3dbf04f
akka97 <akapellari@yahoo.com>
2023-02-24 02:46:25 -0800

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
-* _isuper - check for uppercase character
+* _isupper - check for uppercase character
 * @c: integer parameter
 * Return: Always one, otherwise zero.
 */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-
-	return (1);
+		return (1);
 
 return (0);
 }
