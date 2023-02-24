47c9a8c57408b78151800d274d3d7b164694aa49
akka97 <akapellari@yahoo.com>
2023-02-24 05:05:55 -0800

    morenumbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,17 +2,22 @@
 /**
 * more_numbers - printing more numbers
 *
-*  
+*
 */
 void more_numbers(void)
 {
-	int x, y;
+	int x, y, fn, sn;
 
 	for (x = 0; x < 10; x++)
+
 	{
 		for (y = 0; y < 15; y++)
 		{
-			_putchar(y + '0');
+			fn = y / 10;
+			sn = y % 10;
+			if (y >= 10)
+				_putchar(fn + '0');
+			_putchar(sn + '0');
 		}
 		_putchar('\n');
 	}
