2c9c1e3f36eaefa4f2e6268e60cc74a09a1e15e2
akka97 <akapellari@yahoo.com>
2023-02-24 03:19:48 -0800

    isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _isdigit - check for a digit
+* @c: integer parameter
+* Return: One in case a digit, otherwise zero.
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+return(0);
+}
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isupper - check for uppercase character
+* @c: integer parameter
+* Return: Always one, otherwise zero.
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+return (0);
+}
