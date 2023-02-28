d57759011f513e7e6fca72d8e80fc2d50c20d093
akka97 <akapellari@yahoo.com>
2023-02-28 02:23:31 -0800

    revstring

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -5,18 +5,22 @@
 */
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int fcounter = 0;
-	int i;
+	char tmp;
+	int i, length1, length2;
 
-	while (s[fcounter] != '\0')
-		fcounter++;
+	length1 = 0;
+	length2 = 0;
 
-	for (i = 0; i < fcounter; i++)
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
 	{
-			fcounter--;
-			rev = s[i];
-			s[i] = s[fcounter];
-			s[fcounter] = rev;
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
 	}
+
 }
