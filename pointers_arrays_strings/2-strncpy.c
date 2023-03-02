2aa45cb85d97af949b84d87f2f29e6c396ac2b9e
akka97 <akapellari@yahoo.com>
2023-03-02 00:53:39 -0800

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: counting string
+ * Return: string length
+ */
+
+int_strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
