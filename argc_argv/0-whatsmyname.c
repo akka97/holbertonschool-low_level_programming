dcba653de00693622979946867a897a0e35c7799
akka97 <akapellari@yahoo.com>
2023-03-08 02:06:11 -0800

    whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always zero
+ */
+
+int main(int argc __attribute__((unused)), cahr *argv[])
+{
+	printf("%s\n", *argv);
+	
+	return (0);
+}
