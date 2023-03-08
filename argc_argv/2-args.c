c07fe9b03503fb6b79138cb9b46d169ca5fcb618
akka97 <akapellari@yahoo.com>
2023-03-08 02:22:55 -0800

    args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
