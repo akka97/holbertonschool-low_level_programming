8ba94fa798580b639230d40b2fef3c813966e0aa
akka97 <akapellari@yahoo.com>
2023-03-08 03:21:49 -0800

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a function to prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0, num, i, j, k;
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", "Error");
+
+				return (1);
+			}
+		}
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+
+}
