16ba76ccbe311cb5caebbc7a11aa09ddec0029e4
akka97 <akapellari@yahoo.com>
2023-03-13 11:37:25 -0700

    malloc

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _strdup - function that returns a pointer to a new string
+* @str: pointer
+* Return: Nothing.
+*/
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *dup;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	dup = malloc((len + 1) * sizeof(char));
+
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		dup[i] = str[i];
+	}
+
+	dup[len] = '\0';
+
+	return (dup);
+}
