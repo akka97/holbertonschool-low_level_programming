67951118c86179a25fc7fd805099ee176dadca36
akka97 <akapellari@yahoo.com>
2023-03-16 03:15:15 -0700

    first task

--- preprocessor/0-object_like_macro.h
+++ preprocessor/0-object_like_macro.h
@@ -1,8 +1,8 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
-/**
+/*
 * Desc: Header file that defines a macro named SIZE
-* 	Size as an abbrevation for the token 1024
+* Size as an abbrevation for the token 1024
 */
 
 
