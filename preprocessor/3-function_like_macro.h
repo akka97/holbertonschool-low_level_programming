40b79fb9f9214da2dd1359c2147d4ec2ec3955ba
akka97 <akapellari@yahoo.com>
2023-03-16 05:52:27 -0700

    fourth task

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,9 +1,8 @@
 #ifndef _FUNCTION_LIKE_MACRO
 #define _FUNCTION_LIKE_MACRO
 /**
- * main - computes the absolute value of a number
- * x: number that his value needs to be computed
- * Return: Always zero
+ * ABS(X) - computes the absolute value of a number
+ * @x: number that his value needs to be computed
  */
 
 #define ABS(x) (((x) < 0) ? -(x) : (x))
