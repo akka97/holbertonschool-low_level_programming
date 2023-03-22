28cfd421890f1da6774170bdf7245d1f9775e3a2
akka97 <akapellari@yahoo.com>
2023-03-22 01:41:04 -0700

    task

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -9,7 +9,7 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	
+
 	if (size <= 0)
 		return (-1);
 
@@ -19,6 +19,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 			if (cmp(array[i]) == 1)
 				return (i);
 	}
-	
+
 	return (-1);
 }
