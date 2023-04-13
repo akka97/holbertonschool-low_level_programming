dfa0c90783e9973fd725d774c0f2f1d8bde57327
akka97 <akapellari@yahoo.com>
2023-04-13 03:41:28 -0700

    task

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hash table
+ * @size: table size
+ * Return: table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+		return (NULL);
+	return (table);
+}
